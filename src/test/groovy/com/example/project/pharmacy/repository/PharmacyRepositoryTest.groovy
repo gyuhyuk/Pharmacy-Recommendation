00d49842021162c6f3b7e7fa8f6799b6faa70bf4
gyuhyuk <bruno3@naver.com>
2025-02-28 20:59:20 +0900

    test : PharmacyRepository save 기능 테스트

---
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -0,0 +1,34 @@
+package com.example.project.pharmacy.repository
+
+import com.example.project.AbstractIntegrationContainerBaseTest
+import com.example.project.pharmacy.domain.Pharmacy
+import org.springframework.beans.factory.annotation.Autowired
+
+class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
+    @Autowired
+    private PharmacyRepository pharmacyRepository
+
+    def "PharmacyRepository save"() {
+        given:
+        String address = "서울특별시 노원구 상계동"
+        String name = "우성 약국"
+        double latitude = 37.66
+        double longitude = 127.06
+
+        def pharmacy = Pharmacy.builder()
+        .pharmacyAddress(address)
+        .pharmacyName(name)
+        .latitude(latitude)
+        .longitude(longitude)
+        .build()
+
+        when:
+        def result = pharmacyRepository.save(pharmacy)
+
+        then:
+        result.getPharmacyAddress() == address
+        result.getPharmacyName() == name
+        result.getLatitude() == latitude
+        result.getLongitude() == longitude
+    }
+}
