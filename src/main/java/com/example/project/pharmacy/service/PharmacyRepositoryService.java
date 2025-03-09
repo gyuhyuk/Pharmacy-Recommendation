2ca716a2abdb9dd2756ab075a791511e2d7d95c5
gyuhyuk <bruno3@naver.com>
2025-03-09 22:34:06 +0900

    test : pharmacyService dirty checking 테스트 코드 작성

---
+++ src/main/java/com/example/project/pharmacy/service/PharmacyRepositoryService.java
@@ -0,0 +1,39 @@
+package com.example.project.pharmacy.service;
+
+import com.example.project.pharmacy.domain.Pharmacy;
+import com.example.project.pharmacy.repository.PharmacyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PharmacyRepositoryService {
+    private final PharmacyRepository pharmacyRepository;
+
+    @Transactional
+    public void updateAddress(Long id, String address) {
+        Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
+
+        if(Objects.isNull(entity)) {
+            log.error("[PharmacyRepositoryService updateAddress] not found id: {}", id);
+            return;
+        }
+
+        entity.changePharmacyAddress(address);
+    }
+    public void updateAddressWithoutTransaction(Long id, String address) {
+        Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
+
+        if(Objects.isNull(entity)) {
+            log.error("[PharmacyRepositoryService updateAddress] not found id: {}", id);
+            return;
+        }
+
+        entity.changePharmacyAddress(address);
+    }
+}
