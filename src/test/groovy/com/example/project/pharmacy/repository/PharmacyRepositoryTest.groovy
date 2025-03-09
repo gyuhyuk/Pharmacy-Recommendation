f0746b6c13b5f89ab431a98ecd2ec89d7add29b0
gyuhyuk <bruno3@naver.com>
2025-03-09 23:16:14 +0900

    test : BaseEntity test 코드 생성

--- src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -4,6 +4,8 @@ import com.example.project.AbstractIntegrationContainerBaseTest
 import com.example.project.pharmacy.domain.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 
+import java.time.LocalDateTime
+
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private PharmacyRepository pharmacyRepository
@@ -56,5 +58,22 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
         then:
         result.size() == 1
+    }
+
+    def "BaseEntity 등록"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울특별시 성북구 종암동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder().pharmacyAddress(address).pharmacyName(name).build()
+
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
     }
 }
