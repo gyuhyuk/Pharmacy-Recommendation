04c596767b94f4f08b3be9abaa17a232cdfb5f51
gyuhyuk <bruno3@naver.com>
2025-04-11 19:43:39 +0900

    feat : buildUriByCategorySearch 서비스 코드 추가

--- src/main/java/com/example/project/api/service/KakaoUriBuilderService.java
+++ src/main/java/com/example/project/api/service/KakaoUriBuilderService.java
@@ -11,14 +11,32 @@ import java.net.URI;
 public class KakaoUriBuilderService {
 
     private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+    private static final String KAKAO_LOCAL_CATEGORY_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/category.json";
 
-    public URI buildUrlByAddressSearch(String address) {
+    public URI buildUriByAddressSearch(String address) {
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
 
         uriBuilder.queryParam("query", address);
 
         URI uri = uriBuilder.build().encode().toUri();
         log.info("[KakaoUriBuilderService buildUrlByAddressSearch] address: {}, uri: {}", address, uri);
+
+        return uri;
+    }
+
+    public URI buildUriByCategorySearch(double latitude, double longitude, double radius, String category) {
+        double meterRadius = radius * 1000;
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(KAKAO_LOCAL_CATEGORY_ADDRESS_URL);
+        uriBuilder.queryParam("category_group_code", category);
+        uriBuilder.queryParam("x", longitude);
+        uriBuilder.queryParam("y", latitude);
+        uriBuilder.queryParam("radius", meterRadius);
+        uriBuilder.queryParam("sort", "distance");
+
+        URI uri = uriBuilder.build().encode().toUri();
+
+        log.info("[KakaoUriSearchService buildUrlByCategorySearch] uri: {}", uri);
 
         return uri;
     }
